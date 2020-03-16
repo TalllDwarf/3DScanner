0669c683adef867a30ce8f42909ee7fa450cb7ea
Mark Booth <markbooth_4@hotmail.co.uk>
2020-03-16 20:41:25 +0000

    Updated Arduino code

--- 3DTurntable/3DTurntable.ino
+++ 3DTurntable/3DTurntable.ino
@@ -3,40 +3,49 @@
 
 #define XPIN A0
 #define YPIN A1
+#define BUTTON 13
 
-#define MAX_STEPS 5
+#define MAX_MOTOR_SPEED 15
+#define STEPS 2049
+#define FULL_REV (STEPS * 7)
 
-#define STEPS 64
+LiquidCrystal lcd(8, 7, 5, 4, 3, 2);
+Stepper stepper(STEPS, 9, 11, 10, 12);
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-Stepper stepper(STEPS, 7, 8, 9, 10);
-
-int X = 0, Y = 0;
+int X = 0;
 char line1[16];
 
 char serialIn = ' ';
 
+unsigned char numberOfTurns = 0;
+unsigned short singleStep = 0;
+
 //The how many steps should each step do
-byte eachStep = 1;
+int eachStep = 1;
+int currentStep = 0;
 
 void setup()
 {
   Serial.begin(9600);
+
+  pinMode(BUTTON, INPUT);
+
+  stepper.setSpeed(15);
   
   //setup lcds column and rows
   lcd.begin(16,2);
 
   //Print hello world
-  lcd.print("3D Turntable");
-  lcd.setCursor(0,1);
-  lcd.print("Connecting to PC");
-
-  while(!Serial)
-  {delay(10);}
-  lcd.clear();
-
-  lcd.setCursor(0,0);
+  lcd.print("  3D Turntable  ");
   Serial.println("3D Turntable");
+}
+
+void StopStepper()
+{
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
 }
 
 void loop()
@@ -49,37 +58,93 @@ void loop()
     {
       case 's':
       case 'S':
-        stepper.step(eachStep);
-        delay(1);
-        Serial.write('D');
+      if(singleStep != 0)
+      {
+        stepper.setSpeed(MAX_MOTOR_SPEED);
+        stepper.step(singleStep);
+        StopStepper();
+        currentStep++;
+        delay(10);
+        lcd.setCursor(0,1);
+        sprintf(line1, "Turn:%-4d/%-4d", currentStep, numberOfTurns);
+        lcd.print(line1);
+
+        if(currentStep >= numberOfTurns)
+          Serial.write('F');
+        else 
+          Serial.write('D');
+      }
       break;
 
-      case 'e':
-      case 'E':
-        while(Serial.available() == 0)
-        {delay(1);}
-        eachStep = Serial.read();
+      case 'r':
+      case 'R':
+        currentStep = 0;
+      break;
+
+      case 'i':
+      case 'I':
+        stepper.setSpeed(MAX_MOTOR_SPEED);
+        stepper.step(singleStep);
+        StopStepper();
+        delay(10);
+        Serial.write('D');
       break;
 
       case 't':
       case 'T':
-        Serial.write(STEPS);
+        while(Serial.available() == 0) {}
+       
+        numberOfTurns = Serial.read();
+        singleStep = FULL_REV / (unsigned short)numberOfTurns;
       break;
     }
   }
   else if(serialIn != 's' && serialIn != 'S')
   {
-    X = analogRead(XPIN);
-    X = min(1024, (X + 10));
-    
-    //Y = analogRead(YPIN);    
-    lcd.setCursor(0,1);
+    if(digitalRead(BUTTON) == 0)
+    {
+      lcd.setCursor(0,1);
+      sprintf(line1, "Full rotation  ");
+      lcd.print(line1);
+      
+      stepper.setSpeed(MAX_MOTOR_SPEED);
+      stepper.step(FULL_REV);
+      StopStepper();
 
-    char steps = map(X, 10, 1024, -MAX_STEPS, MAX_STEPS);
+      lcd.setCursor(0,1);
+      sprintf(line1, "Finished       ");
+      lcd.print(line1);
+    }
+    else
+    {
+      X = analogRead(XPIN);
+      X = min(1024, (X + 10));
+  
+      char steps = map(X, 10, 1024, -MAX_MOTOR_SPEED, MAX_MOTOR_SPEED);
+  
+      while(steps != 0)
+      {
+        lcd.setCursor(0,1);
+        sprintf(line1, "Speed:%-3d%", steps);
+        lcd.print(line1);
+        stepper.setSpeed(abs(steps));
 
-    sprintf(line1, "Step:%-2d X:%-4d", steps, X);
-    
-    lcd.print(line1);
+        if(steps > 0)
+          stepper.step(200);
+        else
+          stepper.step(200);
+
+        X = analogRead(XPIN);
+        X = min(1024, (X + 10));
+        steps = map(X, 10, 1024, -MAX_MOTOR_SPEED, MAX_MOTOR_SPEED);  
+      }
+      
+        delay(100 - abs(steps)); 
+        StopStepper();  
+        lcd.setCursor(0,1);
+        sprintf(line1, "Waiting        ");
+        lcd.print(line1); 
+    }
     delay(10);
   }
 }
